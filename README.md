5c4c07da6aa5dd2998a88d86016cc5ebcc036fd7
gnosnah <song.han.gm@gmail.com>
2022-11-21 11:18:03 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # dnstress
+
+A simple dns query stress tool built on github.com/miekg/dns.
+
+Use alexa top 1 million as query domain source. 
+
+top1m: http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
+
+## build
+```
+go build -o dnstress
+```
+
+## usage
+```
+Simple DNS stress tool
+Options:
+  -debug
+        Show debug info (default false)
+  -domainNum int
+        How many domain names to use in the test (default 1)
+  -port string
+        DNS server Port to test (default "53")
+  -srv string
+        DNS server IP address to stress (default "127.0.0.1")
+  -timeout int
+        UDP timeout (seconds, default 5s) (default 5)
+  -workerNum int
+        Number of simultaneous test workers to run (default 1)
+
+```
+
+## output example
+```
+./dnstress -srv 8.8.8.8 -port 53 -workerNum 4 -domainNum 2000 -timeout 3            
+got 648362 domains
+stress...
+result: Queries total:2000, succeed:1988, failed:12,  success rate:0.9940, elapsed:59.433(s)
+```
+
---
+++ srcdata.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"encoding/csv"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	alexaDir  = "https://s3.amazonaws.com/alexa-static"
+	alexaFile = "top-1m.csv.zip"
+	dataDir   = "top1m"
+)
+
+func getTop1mFileFromAWS() ([]byte, error) {
+	fileName := filepath.Join(dataDir, alexaFile)
+	if _, err := os.Stat(fileName); errors.Is(err, os.ErrNotExist) {
+		resp, err := http.Get(alexaDir + "/" + alexaFile)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		err = os.MkdirAll(dataDir, 0777)
+		if err != nil {
+			return nil, err
+		}
+
+		err = os.WriteFile(fileName, b, 0644)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return os.ReadFile(fileName)
+}
+
+func parseTop1mFile(data []byte) ([]string, error) {
+	var domains []string
+
+	r, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		return nil, err
+	}
+
+	fn := func(f *zip.File) error {
+		fr, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer fr.Close()
+
+		cr := csv.NewReader(fr)
+
+		records, err := cr.ReadAll()
+		if err != nil {
+			return err
+		}
+
+		for _, record := range records {
+			domain := record[1]
+			domains = append(domains, domain)
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		if f.Name != strings.TrimSuffix(alexaFile, ".zip") {
+			continue
+		}
+		err = fn(f)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return domains, nil
+}
+
+func GetTop1mDomains() (domains []string, err error) {
+	buf, err := getTop1mFileFromAWS()
+	if err != nil {
+		return nil, err
+	}
+
+	domains, err = parseTop1mFile(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return domains, nil
+}
